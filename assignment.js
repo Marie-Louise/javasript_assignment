cabc40f7fd4f1d8dc69944663ee73dc114048e55
Marie-Louise <mariemuteteyimana@gmail.com>
2021-03-21 20:10:03 +0300

    assignment4

---
+++ assignment.js
@@ -0,0 +1,24 @@
+getGrades()
+function getGrades(){
+
+}
+    var getGrades="one";
+
+if(getGrades>=80){
+    console.log("A");
+    }
+    else if(getGrades>=70){
+       console.log("B");
+    }
+else if(getGrades>=60){
+    console.log("C");
+}
+else if(getGrades>=50){
+    console.log("D");
+}
+else if(getGrades<=49){
+ console.log("E");
+}
+else if(getGrades>="F"){
+    console.log("F");
+}
